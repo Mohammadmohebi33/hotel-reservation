1dc80d2e9840ecf1736eaf0efb3a199cafa03e01
Mohammadmohebi33 <mohammad.rasoul.mohebi@gmail.com>
2024-10-15 13:28:01 +0330

    delete user

--- api/user_handler.go
+++ api/user_handler.go
@@ -37,6 +37,20 @@ func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
 	return c.JSON(users)
 }
 
+//func (h *UserHandler) HandlePutUser(c *fiber.Ctx) error {
+//
+//}
+
+func (h *UserHandler) HandleDeleteUser(c *fiber.Ctx) error {
+	userID := c.Params("id")
+
+	if err := h.userStore.DeleteUser(c.Context(), userID); err != nil {
+		return err
+	}
+
+	return c.JSON(map[string]string{"deleted": userID})
+}
+
 func (h *UserHandler) HandlPostUser(c *fiber.Ctx) error {
 	var params types.CreateUserParams
 
